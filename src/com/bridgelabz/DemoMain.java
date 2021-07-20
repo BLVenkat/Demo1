7a5bf4d49d8131fa127a9dd4857d03a8888faf53
BLVenkat <venkatreddy.mulaa@gmail.com>
2021-07-20 08:46:40 +0530

    [venkat] :Refactor all stories are completed

--- src/com/bridgelabz/DemoMain.java
+++ src/com/bridgelabz/DemoMain.java
@@ -3,6 +3,6 @@ package com.bridgelabz;
 public class DemoMain {
 
 	public static void main(String[] args) {
-		System.out.println("welcome all");
+		System.out.println("every thing is working fine");
 	}
 }
